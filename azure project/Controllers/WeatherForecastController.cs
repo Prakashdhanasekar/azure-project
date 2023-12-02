7af9f5b3ed5c969e48483031a4608ffb8df2debd
prakash.d <prakash.d@saint-gobain.com>
2023-12-02 11:57:16 +0530

    Azure sample test

--- azure project/Controllers/WeatherForecastController.cs
+++ azure project/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace azure_project.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Test Azure"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
